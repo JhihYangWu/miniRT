53e693bbf110f2ee1dcff34c0765fc99a17b9368
JhihYang Wu <jhihyang_wu@outlook.com>
2024-05-05 16:31:25 -0700

    add vector3 class

---
+++ src/mymath/vector3.cpp
@@ -0,0 +1,129 @@
+#include "vector3.hpp"
+#include <cassert>
+#include <math.h>
+#include <sstream>
+#include <iomanip>
+
+Vector3::Vector3(float x, float y, float z) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+Vector3::Vector3(float val) {
+    this->x = val;
+    this->y = val;
+    this->z = val;
+}
+
+Vector3::Vector3(const Vector3& other) {
+    this->x = other.x;
+    this->y = other.y;
+    this->z = other.z;
+}
+
+Vector3& Vector3::operator=(const Vector3& other) {
+    this->x = other.x;
+    this->y = other.y;
+    this->z = other.z;
+    return *this;
+}
+
+float Vector3::operator[](int index) {
+    assert(index >= 0 && index <= 2);
+    return data[index];
+}
+
+float Vector3::len() {
+    return sqrt(this->lenSquared());
+}
+
+float Vector3::lenSquared() {
+    return x * x + y * y + z * z;
+}
+
+Vector3& Vector3::normalize() {
+    float length = this->len();
+    if (length != 0) {
+        this->x /= length;
+        this->y /= length;
+        this->z /= length;
+    }
+    return *this;
+}
+
+std::string Vector3::str() {
+    std::stringstream ss;
+    ss << "<" << std::fixed << std::setprecision(2) << this->x << ", " << this->y << ", " << this->z << ">";
+    return ss.str();
+}
+
+float dot(const Vector3& v0, const Vector3& v1) {
+    return v0.x * v1.x + v0.y * v1.y + v0.z * v1.z;
+}
+
+Vector3 cross(const Vector3& v0, const Vector3& v1) {
+    return Vector3(
+        v0.y * v1.z - v0.z * v1.y,
+        v0.z * v1.x - v0.x * v1.z,
+        v0.x * v1.y - v0.y * v1.x
+    );
+}
+
+Vector3 normalize(Vector3& v) {
+    float length = v.len();
+    if (length == 0) return v;
+    return v / length;
+}
+
+Vector3 operator+(const Vector3& v0, const Vector3& v1) {
+    return Vector3(v0.x + v1.x, v0.y + v1.y, v0.z + v1.z);
+}
+
+Vector3 operator-(const Vector3& v0, const Vector3& v1) {
+    return Vector3(v0.x - v1.x, v0.y - v1.y, v0.z - v1.z);
+}
+
+Vector3 operator*(const Vector3& v0, const Vector3& v1) {
+    return Vector3(v0.x * v1.x, v0.y * v1.y, v0.z * v1.z);
+}
+
+Vector3 operator/(const Vector3& v0, const Vector3& v1) {
+    return Vector3(v0.x / v1.x, v0.y / v1.y, v0.z / v1.z);
+}
+
+Vector3 operator+(const Vector3& v, const float scalar) {
+    return Vector3(v.x + scalar, v.y + scalar, v.z + scalar);
+}
+
+Vector3 operator-(const Vector3& v, const float scalar) {
+    return Vector3(v.x - scalar, v.y - scalar, v.z - scalar);
+}
+
+Vector3 operator*(const Vector3& v, const float scalar) {
+    return Vector3(v.x * scalar, v.y * scalar, v.z * scalar);
+}
+
+Vector3 operator/(const Vector3& v, const float scalar) {
+    return Vector3(v.x / scalar, v.y / scalar, v.z / scalar);
+}
+
+Vector3 operator+(const float scalar, const Vector3& v) {
+    return Vector3(scalar + v.x, scalar + v.y, scalar + v.z);
+}
+
+Vector3 operator-(const float scalar, const Vector3& v) {
+    return Vector3(scalar - v.x, scalar - v.y, scalar - v.z);
+}
+
+Vector3 operator*(const float scalar, const Vector3& v) {
+    return Vector3(scalar * v.x, scalar * v.y, scalar * v.z);
+}
+
+Vector3 operator/(const float scalar, const Vector3& v) {
+    return Vector3(scalar / v.x, scalar / v.y, scalar / v.z);
+}
+
+const Vector3 operator-(const Vector3& v) {
+    return Vector3(-v.x, -v.y, -v.z);
+}
---
+++ src/mymath/vector3.hpp
@@ -0,0 +1,58 @@
+#ifndef VECTOR3_H
+#define VECTOR3_H
+
+#include <string>
+
+class Vector3{
+public:
+    // constructors
+    Vector3(float x, float y, float z);
+    Vector3(float val); // x = y = z = val
+    Vector3(const Vector3& other); // copy constructor
+
+    // operators and methods
+    Vector3& operator=(const Vector3& other);
+    float operator[](int index);
+    float len();
+    float lenSquared();
+    Vector3& normalize();
+    std::string str();
+
+    // attributes
+    // the 3 float in Vector3 can be accessed in different ways
+    union{
+        struct{
+            float x, y, z;
+        };
+        struct{
+            float r, g, b;
+        };
+        struct{
+            float data[3];
+        };
+    };
+};
+
+// operators and functions
+float dot(const Vector3& v0, const Vector3& v1);
+Vector3 cross(const Vector3& v0, const Vector3& v1);
+Vector3 normalize(Vector3& v);
+
+Vector3 operator+(const Vector3& v0, const Vector3& v1);
+Vector3 operator-(const Vector3& v0, const Vector3& v1);
+Vector3 operator*(const Vector3& v0, const Vector3& v1); // element wise multiplication
+Vector3 operator/(const Vector3& v0, const Vector3& v1); // element wise division
+
+Vector3 operator+(const Vector3& v, const float scalar);
+Vector3 operator-(const Vector3& v, const float scalar);
+Vector3 operator*(const Vector3& v, const float scalar);
+Vector3 operator/(const Vector3& v, const float scalar);
+
+Vector3 operator+(const float scalar, const Vector3& v);
+Vector3 operator-(const float scalar, const Vector3& v);
+Vector3 operator*(const float scalar, const Vector3& v);
+Vector3 operator/(const float scalar, const Vector3& v);
+
+const Vector3 operator-(const Vector3& v); // negate vector
+
+#endif
